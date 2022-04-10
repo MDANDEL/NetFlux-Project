e37daf5185166f680df5dd4f15ddeb2af9fe01fa
MDANDEL <92440390+MDANDEL@users.noreply.github.com>
2022-04-10 22:23:38 +0200

    first commit of clone's netflix project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2 id="popular-title">Popular</h2>
+  <div id="popular">
+    <div class="row">
+      <Image
+        v-for="movie in popular_movies"
+        v-bind:key="movie"
+        :img="movie.img"
+        :name="movie.name"
+        v-on:click="getPopularMovies()"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Image from "./components/ImageMovie.vue"
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Image
+  },
+  methods: {
+    async getPopularMovies() {
+      let response = await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=a7ac5b024a2bd5fe31e06534b18a0b26`
+      );
+      let data = response.json();
+      for (let i = 0; i < data.length; i++) {
+        this.popular_movies.push({
+          img: `https://image.tmdb.org/t/p/original${data[i].poster_path}`,
+          name: data[i].title,
+          id: data[i].id,
+        });
+      }
+    },
+
+    async created() {
+      await this.getPopularMovies();
+    },
+  },
+};
 </script>
 
 <style>
